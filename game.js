4ddc978bea45f739f89f5820f49cd8dc6f6985b3
Juan-var-camp <vargascampuzanojuanjose@gmail.com>
2026-01-27 13:15:43 -0500

    Update game.js
    djfg

--- game.js
+++ game.js
@@ -170,8 +170,8 @@ function loop() {
     
     ctx.font = "18px Arial";
     ctx.fillStyle = "#fff";
-    ctx.fillText("Nicolle", canvas.width / 2, canvas.height / 2 + 15);
-    ctx.fillText("'________", canvas.width / 2, canvas.height / 2 + 40);
+    ctx.fillText("Nicolle Perez tengo 18 años", canvas.width / 2, canvas.height / 2 + 15);
+    ctx.fillText("De Cucuta", canvas.width / 2, canvas.height / 2 + 40);
     
     ctx.font = "bold 20px Arial";
     ctx.fillStyle = "#ffff00";
